04013dd54bbf3490cbdc774a78deb7a383c5c551
M-Akthar <Muhammad.Aktharuzzaman@academytrainee.com>
2019-07-19 13:44:00 +0100

    added a task controller

---
+++ src/main/java/rest/TaskController.java
@@ -0,0 +1,41 @@
+package rest;
+
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+import service.TaskService;
+
+@Path("/task")
+public class TaskController {
+
+	@Inject
+	private TaskService service;
+	
+	@GET
+	@Path("/getAll")
+	public String getAllTasks() {
+		return this.service.getAllTasks();
+	}
+
+	@POST
+	@Path("/update/{id}")
+	public String updateTask(@PathParam("id") long id, String task) {
+		return this.service.updateTask(id, task);
+	}
+
+	@DELETE
+	@Path("/delete/{id}")
+	public String deleteTask(@PathParam("id") long id) {
+		return this.service.deleteTask(id);
+	}
+
+	@POST
+	@Path("/create/{userID}")
+	public String createTask(String task, @PathParam("userID") long userID) {
+		return this.service.createTask(task, userID);
+	}
+}
